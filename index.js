f833391c8d7365bf22295faa5107574356b58b5b
Bruno Santos <marcilio.santos@aluno.ifsp.edu.br>
2023-10-27 15:18:57 -0300

    projeto terminado

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const list = document.querySelector('ul')
 const buttonShow = document.querySelector('.show-all')
-const buttonMap = document.querySelector('.map')
+const buttonMap = document.querySelector('.map-all')
+const buttonReduce = document.querySelector('.sum-all')
+const buttonFilter = document.querySelector('.filter')
 let MyLi = ''
+
+function formatCurrency(value){
+    const newValue = value.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+
+    return newValue
+}
 
 function showAll(productsArray) {
     MyLi = ''
@@ -11,7 +22,7 @@ function showAll(productsArray) {
                 <li>
                     <img src=${product.src}>
                     <p>${product.name}</p>
-                    <p class="item-price">R$ ${Math.round(product.price)},00</p>
+                    <p class="item-price">${(formatCurrency(product.price))}</p>
                 </li>
                     ` 
     })
@@ -20,7 +31,7 @@ function showAll(productsArray) {
 
 
 
-function mapList (){
+function mapList () {
     const NewPrices = menuOptions.map((product) => ({
         ...product,
         price: product.price * 0.9 
@@ -29,7 +40,22 @@ function mapList (){
     showAll(NewPrices)
 }
 
+function reduceList(){
+    const finalValue = menuOptions.reduce( (acc, current) => acc + current.price, 0)
+    list.innerHTML = `
+    <li> 
+        <p>O valor final dos itens é ${formatCurrency(finalValue)}</p>
+    </li>
+   `
+}
 
+function filterList (){
+    const filterVegan = menuOptions.filter((product) => product.vegan) // (product) => product.vegan) é a mesma coisa que: (product) => product.vegan === true
+
+    showAll(filterVegan)
+}
 
 buttonShow.addEventListener('click', () => showAll(menuOptions)) //colocar uma funcao anonima na frente para o programa nao starta junto a funcao
 buttonMap.addEventListener('click', mapList)
+buttonReduce.addEventListener('click', reduceList)
+buttonFilter.addEventListener('click', filterList)
